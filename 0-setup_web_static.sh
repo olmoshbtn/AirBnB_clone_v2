ee8135d18dff266c1f974b3601ad222d08f28052
olmoshbtn <2120@holbertonschool.com>
2021-04-15 01:45:15 +0000

    0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Install Nginx and configure it
+
+apt-get -y update
+apt-get -y install nginx
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+        <head>
+                <title>Holberton School</title>
+        </head>
+        <body>
+                <h1>Holberton School</h1>
+        </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i "/server_name _;/a location /hbnb_static/ {\\n\\talias /data/web_static/current/;\\n\\t}\\n" /etc/nginx/sites-available/default
+service nginx restart
