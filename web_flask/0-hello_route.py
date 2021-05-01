de39feb24a3339ced6ba86097abf55e6764cfb80
olmoshbtn <2120@holbertonschool.com>
2021-05-01 20:49:23 +0000

    2-c_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,8 +12,8 @@ def index():
     """
     Site Index
     """
-    return 'Hello HBNB!'
+    return  "Hello HBNB!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
