cea72e18c42f5687e0fab94e7aec8d0a202eb99c
olmoshbtn <2120@holbertonschool.com>
2021-05-01 21:39:52 +0000

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
-from flask import Flask
+"""
+Start a Flask web application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Display Hello"""
-    return 'Hello HBNB!'
+def index():
+    """
+    Site Index
+    """
+    return  "Hello HBNB!"
 
-if __name__ == '__main__':
-    app.run()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
