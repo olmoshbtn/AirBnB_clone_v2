7c1ce6cc53b03e3eaca2dbe56a5c106bcf93717a
olmoshbtn <2120@holbertonschool.com>
2021-05-01 18:04:00 +0000

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,12 +7,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False):
-    def index():
-        """
-        Site Index
-        """
-        return 'Hello HBTN!'
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Site Index
+    """
+    return 'Hello HBTN!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
