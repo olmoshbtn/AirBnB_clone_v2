9e90b6939050c21da2458bf919ee04f5843d28d8
olmoshbtn <2120@holbertonschool.com>
2021-05-02 16:22:54 +0000

    9-states.py

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Start a Flask web application
+"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id=None):
+    """
+    list states or show details of a particular state
+    """
+    template = '9-states.html'
+    states = storage.all(State)
+    key = None if id is None else 'State.{id}'.format(id=id)
+    return render_template(template, states=states, key=key)
+
+
+@app.teardown_appcontext
+def close_storage(exc):
+    """
+    close storage
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
