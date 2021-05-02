5cad86100ff848ca3972a31f5c05e8b8b74ac8c6
olmoshbtn <2120@holbertonschool.com>
2021-05-02 02:33:41 +0000

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -45,7 +45,7 @@ def number_n(n):
     """
     Display 'n is a number', only if n is an integer
     """
-    return '{} is a number'.format
+    return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
